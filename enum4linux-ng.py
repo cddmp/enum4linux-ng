02f27ebad42a19f29bc0b37ce1d5b8471ad890bc
xiaorui@JRZ <33048622+JohnRyk@users.noreply.github.com>
2021-08-11 22:01:02 +0800

    Update enum4linux-ng.py
    Fix a keyword arguments "sort_keys=True" in yaml.dump()

--- enum4linux-ng.py
+++ enum4linux-ng.py
@@ -2550,7 +2550,7 @@ def warn(msg):
     print("\n"+Colors.yellow(f"[!] {msg}"))
 
 def yamlize(msg, sort=False, rstrip=True):
-    result = yaml.dump(msg, default_flow_style=False, sort_keys=sort, width=160, Dumper=Dumper)
+    result = yaml.dump(msg, default_flow_style=False, sort_keys=True, width=160, Dumper=Dumper)
     if rstrip:
         return result.rstrip()
     return result
