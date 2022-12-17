9429b2ae2421a40aa0d338e2a0a493b5aa323da2
cddmp <cddmp@users.noreply.github.com>
2022-12-17 10:22:12 +0100

    Update Windows releases

--- enum4linux-ng.py
+++ enum4linux-ng.py
@@ -200,6 +200,9 @@ OS_VERSIONS = {
 
 # Source: https://docs.microsoft.com/de-de/windows/release-health/release-information
 OS_RELEASE = {
+        "19045": "22H2",
+        "19044": "21H2",
+        "19043": "21H1",
         "19042": "20H2",
         "19041": "2004",
         "18363": "1909",
